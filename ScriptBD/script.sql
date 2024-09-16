70e2fe79cd221cc6d93f40ff5668094aa72aca38
chrisra <chrisralopez@gamil.com>
2024-09-16 16:03:33 -0600

    Se agregaron registros en el svript de la Base de datos

--- ScriptBD/script.sql
+++ ScriptBD/script.sql
@@ -8,3 +8,10 @@ nombre VARCHAR(255) NOT NULL,
 descripcion VARCHAR(255),
 precio DOUBLE NOT NULL
 );
+
+INSERT INTO productos VALUES
+(NULL, 10000000001, 'Coca Cola', 'Bebida de Cola', 19.99),
+(NULL, 10000000002, 'Pepsi', 'Bebida de Cola', 18.99),
+(NULL, 10000000003, 'Laptop', 'Bebida de Cola', 19999.99),
+(NULL, 10000000004, 'Doritos', 'Bebida de Cola', 18.99),
+(NULL, 10000000005, 'Cheetos', 'Bebida de Cola', 13.99);
